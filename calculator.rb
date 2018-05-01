eb63c5e8aea1d3ebb01b8e10e027e41992ed30f1
Karen <kjgamis@live.ca>
2018-04-30 23:22:01 -0400

    done

---
+++ calculator.rb
@@ -0,0 +1,15 @@
+def add(num1, num2)
+  return num1 + num2
+end
+
+def subtract(num1, num2)
+  return num1 - num2
+end
+
+def sum(array)
+  total = 0
+  array.each do |num|
+    total += num
+  end
+  return total
+end
